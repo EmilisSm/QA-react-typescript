6aee45f4b21fac23a50b0fd797d65bf47739a538
Emil <emil.simanski@gmail.com>
2022-02-04 20:16:49 +0100

    Fix

--- src/components/organisms/LeftSide.tsx
+++ src/components/organisms/LeftSide.tsx
@@ -4,8 +4,8 @@ import { selectQuestions } from '../../redux/questionsSlice';
 export const LeftSideSection: React.FC = () => {
   const questions = useAppSelector((state) => selectQuestions(state).questions);
   return (
-    <section>{`Here you can find ${questions.length} question ${
-      questions.length !== 1 && 's'
+    <section>{`Here you can find ${questions.length} question${
+      questions.length !== 1 ? 's' : ''
     }`}</section>
   );
 };
